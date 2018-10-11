ec9a0fcbb806c422b944deb5ef84b899a0fba057
George Hill <georgehill@georges-mbp.home>
2018-10-11 16:12:59 +0100

    BAU Moving outputs into outputs.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,27 +98,3 @@ resource "aws_instance" "myfirstec2" {
     Name = "${var.aws_instance_tag_name}"
   }
 }
-
-output "public_ip" {
- value = "${aws_instance.myfirstec2.public_ip}"
-}
-
-output "id" {
- value = "${aws_instance.myfirstec2.id}"
-}
-
-output "public_dns" {
- value = "${aws_instance.myfirstec2.public_dns}"
-}
-
-output "security_groups" {
- value = "${aws_instance.myfirstec2.security_groups}"
-}
-
-output "subnet_id" {
- value = "${aws_instance.myfirstec2.subnet_id}"
-}
-
-output "VPC_security_group" {
- value = "${aws_instance.myfirstec2.vpc_security_group_ids}"
-}
