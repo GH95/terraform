53ce7c72dc4fc7d222f90fcd16c2b6f047329998
George Hill <georgehill@georges-mbp.home>
2018-10-11 11:23:01 +0100

    BAU: Add output variables and remove access keys (these can be exported)

--- main.tf
+++ main.tf
@@ -1,13 +1,35 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
 }
 
 resource "aws_instance" "myfirstec2" {
   ami = ""
   instance_type = "t2.micro"
   	tags {
-	      Name = "terraform-myfirstec2"
+	      Name = "My EC2 Instance"
 	}
+}
+
+output "public_ip" {
+ value = "${aws_instance.myfirstec2.public_ip}"
+}
+
+output "id" {
+ value = "${aws_instance.myfirstec2.id}"
+}
+
+output "public_dns" {
+ value = "${aws_instance.myfirstec2.public_dns}"
+}
+
+output "security_groups" {
+ value = "${aws_instance.myfirstec2.security_groups}"
+}
+
+output "subnet_id" {
+ value = "${aws_instance.myfirstec2.subnet_id}"
+}
+
+output "VPC_security_group" {
+ value = "${aws_instance.myfirstec2.vpc_security_group_ids}"
 }
